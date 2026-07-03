e9fec8a88b6eb81bf611e2c50522419497b7f650
timelessnesses <mooping@timelessnesses.me>
2026-07-03 14:57:15 +0700

    ay

---
+++ src/routes/admin/users/[user]/delete/+server.ts
@@ -0,0 +1,33 @@
+import { json } from "@sveltejs/kit";
+
+export const POST = async ({ params, platform, locals }) => {
+    if (!locals.user) {
+        return json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    if (!locals.user.admin) {
+        return json({ error: 'Forbidden' }, { status: 403 });
+    }
+
+    const userEmail = params.user;
+
+    if (!userEmail) {
+        return json({ error: 'User email is required' }, { status: 400 });
+    }
+
+    const env = platform?.env as Env | undefined;
+
+
+    try {
+        await env?.AccountingDatabase.prepare('DELETE FROM users WHERE email = ?').bind(userEmail).run();
+        await env?.AccountingDatabase.prepare('INSERT INTO logs (email, action, timestamp) VALUES (?, ?, ?)').bind(
+            locals.user.email,
+            `Admin ${locals.user.email} deleted user with email: ${userEmail}`,
+            Math.floor(Date.now() / 1000)
+        ).run();
+        return json({ ok: true });
+    } catch (e) {
+        console.error('Error deleting user:', e);
+        return json({ error: 'Error deleting user' }, { status: 500 });
+    }
+}
